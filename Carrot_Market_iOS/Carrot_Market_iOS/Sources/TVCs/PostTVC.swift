bb774e331ca347e69793825cf3ae28b1b41b1809
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-11-07 21:07:17 +0900

    ➕[ADD] : TableViewCell UI 구성, 데이터 구조체 생성 (#1)

---
+++ Carrot_Market_iOS/Carrot_Market_iOS/Sources/TVCs/PostTVC.swift
@@ -0,0 +1,47 @@
+//
+//  PostTVC.swift
+//  Carrot_Market_iOS
+//
+//  Created by 정은희 on 2021/11/07.
+//
+
+import UIKit
+
+class PostTVC: UITableViewCell {
+    
+    static let identifier = "PostTVC"
+
+    // MARK: - UI Component Part
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var updateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var heartLabel: UILabel!
+    
+    
+    // MARK: - Life Cycle Part
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    // MARK: - Custom Method Part
+    func setData(productData: AppContentData) {
+        // IBOutlets으로 선언했던 변수들에 데이터를 넣어주는 부분
+        
+        productImageView.image = productData.makeImage()
+        
+        nameLabel.text = productData.name
+        priceLabel.text = productData.price
+        heartLabel.text = productData.heart
+        
+        updateLabel.text = "\(productData.dong) · 끌올 \(productData.update)초 전"
+        
+    }
+    
+}
--- Carrot_Market_iOS/Carrot_Market_iOS/Sources/VCs/HomeVC.swift
+++ Carrot_Market_iOS/Carrot_Market_iOS/Sources/VCs/HomeVC.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    // MARK: - UI Component Part
+    @IBOutlet weak var postTableView: UITableView!
+    
+    // MARK: - Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
 
